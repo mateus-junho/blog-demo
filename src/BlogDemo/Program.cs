0d451fff239c2936703e7bb55eeb32f9472c8ba5
Mateus Junho <mateus.junho@farfetch.com>
2023-01-03 17:16:25 +0000

    feat: add models

--- src/BlogDemo/Program.cs
+++ src/BlogDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Models;
+using BlogDemo.Repositories;
 using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 using System;
@@ -10,21 +11,71 @@ namespace BlogDemo
         private const string CONNECTION_STRING = "Server=(localdb)\\mssqllocaldb;Database=Blog;Trusted_Connection=True;MultipleActiveResultSets=true";
 
         static void Main(string[] args)
-        {
-            ReadUsers();
-        }
-
-        public static void ReadUsers()
-        {
+        { 
             using (var connection = new SqlConnection(CONNECTION_STRING))
             {
-                var users = connection.GetAll<User>();
-
-                foreach (var user in users)
-                {
-                    Console.WriteLine(user.Name);
-                }
+                GetUsers(connection);
+                //Getuser();
+                //AddUser();
+                //UpdateUser();
+                //DeleteUser();
             }
+        }
+
+        public static void GetUsers(SqlConnection connection)
+        {
+            var repository = new UserRepository(connection);
+
+            foreach (var user in repository.GetAll())
+            {
+                Console.WriteLine(user.Name);
+            }
+        }
+
+        public static void GetUser(SqlConnection connection, int id)
+        {
+            var user = connection.Get<User>(id);
+            Console.WriteLine(user.Name);
+        }
+
+        public static void AddUser(SqlConnection connection)
+        {
+            var user = new User()
+            {
+                Name = "Mateus Almeida",
+                Email = "mateus@email.com",
+                PasswordHash = "HASH",
+                Bio = "my bio",
+                Image = "https://",
+                Slug = "mateus-almeida",
+            };
+
+            connection.Insert(user);
+            Console.WriteLine("user created");
+        }
+
+        public static void UpdateUser(SqlConnection connection)
+        {
+            var user = new User()
+            {
+                Id = 2,
+                Name = "Mateus Almeida",
+                Email = "mateus@email.com",
+                PasswordHash = "HASH",
+                Bio = "my bio edited",
+                Image = "https://",
+                Slug = "mateus-almeida",
+            };
+
+            connection.Update(user);
+            Console.WriteLine("user updated");
+        }
+
+        public static void DeleteUser(SqlConnection connection)
+        {
+            var user = connection.Get<User>(2);
+            connection.Delete(user);
+            Console.WriteLine("user deleted");
         }
     }
 }
---
+++ src/BlogDemo/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace BlogDemo.Repositories
+{
+    public class UserRepository
+    {
+        private readonly SqlConnection connection;
+
+        public UserRepository(SqlConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public IEnumerable<User> GetAll()
+            => connection.GetAll<User>();
+
+        public User Get(int id)
+            => connection.Get<User>(id);
+
+        public void Add(User user)
+            => connection.Insert<User>(user);
+    }
+}
